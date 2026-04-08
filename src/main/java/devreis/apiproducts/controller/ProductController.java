45e7e21b4d213050109348008a0e61bd5289e86c
Vitor Reis <vitorreis0303@gmail.com>
2026-04-08 00:01:52 -0300

    feat: Implementando a requisição getProductById

--- src/main/java/devreis/apiproducts/controller/ProductController.java
+++ src/main/java/devreis/apiproducts/controller/ProductController.java
@@ -4,10 +4,12 @@ import devreis.apiproducts.dto.ProductDTO;
 import devreis.apiproducts.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
@@ -23,5 +25,11 @@ public class ProductController {
     public ResponseEntity<List<ProductDTO>> getAllProducts(){
         List<ProductDTO> products = service.findAll();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable UUID id){
+        ProductDTO product = service.findById(id);
+        return ResponseEntity.ok(product);
     }
 }

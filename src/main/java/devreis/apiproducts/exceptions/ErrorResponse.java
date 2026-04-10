cc9decb803aa3f0e335da59a36b3182a60a525e4
Vitor Reis <vitorreis0303@gmail.com>
2026-04-10 19:22:35 -0300

    feat: Classe ErrorResponse

---
+++ src/main/java/devreis/apiproducts/exceptions/ErrorResponse.java
@@ -0,0 +1,18 @@
+package devreis.apiproducts.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+    LocalDateTime timestamp;
+}
